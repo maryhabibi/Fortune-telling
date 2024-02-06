c5161c4e8fddc8ab997be6e04be9e0d73caac40e
Maryam Habibi <a.maryamhabibi@gmail.com>
2024-02-05 22:55:13 -0700

    timer change

--- wait.js
+++ wait.js
@@ -8,7 +8,7 @@ function load() {
     }, 1000);
 
     //////automatic redirect
-    setTimeout(function () { window.location.href = 'interpretation.html' }, 6000);
+    setTimeout(function () { window.location.href = 'interpretation.html' }, 5000);
 
 }
 
